6bdb70322683ea9a5207dafe196cc28438521549
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-11-15 17:07:31 -0300

    ADD: JSX archives, add routes and import app itens

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import './scss/global.scss'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+//IMPORT COMPONENTS AND ROUTES
+import Header from './components/Header/Header.jsx';
+import Home from './components/Routes/Home/Home.jsx';
+import DonatedBook from './components/Routes/DonatedBook/DonatedBook.jsx';
+import Donate from './components/Routes/Donate/Donate.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
   return (
-    <div>
-      <h1>Estamos testando o funcionamento</h1>
-    </div>
+    <>
+    <Header />
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <Link to='/'>Home</Link>
+          <Link to='/Donate'> Donate</Link>
+          <Link to='/DonatedBooks'> Donated Books</Link>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Donate' element={<Donate />} />
+        <Route path='/DonatedBooks' element={<DonatedBook />} />
+      </Routes> 
+    </BrowserRouter>
+    <Footer/>
+    </>
   )
 }
 
---
+++ src/components/Routes/DonatedBooks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DonatedBooks() {
+  return (
+    <div>DonatedBooks</div>
+  )
+}
+
+export default DonatedBooks
