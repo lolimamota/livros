ba2510627358a7421dc251583ef3cf48531eb715
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-11-18 23:10:37 -0300

    ADD: Criação da Array de objetos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,30 @@ import '../scss/global.scss';
 import S from '../scss/styleComponents/routes/home/home.module.scss';
 
 function Home() {
+  const vantagens =[
+    {
+      cardImg: Ciranda,
+      cardText: "Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social."
+    },
+    {
+      cardImg: Leitor,
+      cardText: "Estimula o hábito da leitura e o aprendizado contínuo."
+    },
+    {
+      cardImg: Uniao,
+      cardText: "Estimula o hábito da leitura e o aprendizado contínuo."
+    },
+    {
+      cardImg: Balanca,
+      cardText: "Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado."
+    }
+  ]
   return (
     <main>
       <section className={S.sectionBg}>
         <h1>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h1>
       </section>
       <section className={S.sectionCard}>
-        <h2>só pra eu ver ali</h2>
       </section>
     </main>
   )
