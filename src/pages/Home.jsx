a97ebc3894b5fbdf8288eaf2453576caeeb623e4
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-11-18 22:05:26 -0300

    FIX: tipografia, modifiquei conforme a visão mais agradável, ADD: homepage itens

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import '../scss/global.scss';
+
+import S from '../scss/styleComponents/routes/home/home.module.scss';
 
 function Home() {
   return (
-    <div>Home PÁGINA 0</div>
+    <main>
+      <section className={S.sectionBg}>
+        <h1>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h1>
+      </section>
+      <section className={S.sectionCard}>
+        <h2>só pra eu ver ali</h2>
+      </section>
+    </main>
   )
 }
 
