48f1f96272df056c1db798e0341fdcb5565f1e0b
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-11-21 08:25:24 -0300

    ADD: estrutura da página de livros doados

--- src/pages/DonatedBooks.jsx
+++ src/pages/DonatedBooks.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import '../scss/global.scss'
+
+
+//IMPORT STYLE
+import S from '../scss/styleComponents/routes/donated-book/donatedbook.module.scss';
+
+//IMPORT IMAGES
+import LivroDoado from '../assets/routes-img/livrodoado.png'
 
 function DonatedBooks() {
   return (
-    <div>DonatedBooks PÁGINA 1</div>
+    <main>
+      <section>
+        <img src={LivroDoado} alt="Imagem de capa do livro O protagonista de Susanne Andrade" />
+        <h3>O protagonista</h3>
+        <h3>Susanne Andrade</h3>
+        <h3>Ficção</h3>
+      </section>
+    </main>
   )
 }
 
