d5d216575eb78485d49680dfb526921512cb5dd7
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-11-21 08:30:02 -0300

    INIT: estilização da página de livros doados

--- src/pages/DonatedBooks.jsx
+++ src/pages/DonatedBooks.jsx
@@ -10,8 +10,8 @@ import LivroDoado from '../assets/routes-img/livrodoado.png'
 
 function DonatedBooks() {
   return (
-    <main>
-      <section>
+    <main className={S.mainDonated}>
+      <section className={S.sectionDonated}>
         <img src={LivroDoado} alt="Imagem de capa do livro O protagonista de Susanne Andrade" />
         <h3>O protagonista</h3>
         <h3>Susanne Andrade</h3>
