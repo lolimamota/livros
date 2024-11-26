bd4a60ce882d2966205f7e0f15ae0e7f40645d80
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-11-25 21:05:38 -0300

    ADD: API teste para verificar se foi consumida

---
+++ src/livros.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export async function funcLivros() {
+    const getLivros = async () => {
+        const [book, setBook] = useState([]);
+
+        const Livros = await axios.get("https://fakestoreapi.com/products");
+        setBook(Livros.data);
+
+        useEffect(() => {
+            getLivros();
+        }, []);
+    };
+
+    return (
+        <section>
+            {book.map((book) => (
+                <div key={book.id}>
+                    <img src={book.image} alt="" />
+                    <h3>{book.title}</h3>
+                </div>
+            ))}
+        </section>
+    )
+}
