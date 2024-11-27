4a21d4865e5207a6bea93aee990542361001d294
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-11-26 21:43:40 -0300

    RM: instalação do axios e implantação da API book

---
+++ src/livros.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default async function funcLivros() {
+    const [book, setBook] = useState([]);
+    const getLivros = async () => {
+
+        const Livros = await axios.get("https://fakestoreapi.com/products");
+        setBook(Livros.data);
+
+        useEffect(() => {
+            getLivros();
+        }, []);
+    };
+
+    return (
+        <section>
+            {book.map((book) => (
+                <div key={book.id}>
+                    <img src={book.image} alt="" />
+                    <h3>{book.title}</h3>
+                </div>
+            ))}
+        </section>
+    )
+}
