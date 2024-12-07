2228a55711031f4866df9479f15fa079a9d9245d
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-12-07 10:46:35 -0300

    ADD: Library Toast-React

--- src/pages/Routes/Donate.jsx
+++ src/pages/Routes/Donate.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import '../../scss/global.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -11,14 +11,14 @@ import Book from '@assets/routes-img/livro.png'
 
 
 function Donate() {
-  const notify = () =>toast("wow deu bom malandro!")
-  return (
+  const notify = () => toast("wow deu bom malandro!");
 
+  return (
     <main className={S.mainDonate}>
       <section>
-      <h2>
-      Por favor, preencha o formulário com as informações do Livro
-      </h2>
+        <h2>
+          Por favor, preencha o formulário com as informações do Livro
+        </h2>
       </section>
       <section className={S.cardForm}>
         <div className={S.divForm}>
@@ -31,6 +31,7 @@ function Donate() {
           <input className={S.shadowInput} type="text" id="Author" placeholder="Autor" />
           <input className={S.shadowInput} type="text" id="imgLink" placeholder="Link da imagem" />
           <button type='submit' className={S.donateBtn} onClick={notify}>Doar</button>
+          <ToastContainer />
         </form>
       </section>
     </main>
