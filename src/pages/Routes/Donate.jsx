61bd4790ae69817c19b72a492ab8a2b56775e312
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-03-29 16:29:00 -0300

    FIX: API request funcionando

--- src/pages/Routes/Donate.jsx
+++ src/pages/Routes/Donate.jsx
@@ -23,7 +23,7 @@ function Donate() {
 
   // estruturar o envio dos dados
   const sendData = async()=>{
-    const urlAPI = "api-livros-wtvn.onrender.com/donated"
+    const urlAPI = "https://api-livros-wtvn.onrender.com/donate"
 
     const dataTo = {
       title,
