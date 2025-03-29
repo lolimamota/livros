9183e50fc4dbc3867f39e40296abad7781d61ac8
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-03-29 20:08:41 -0300

    FIX: get methods funcional, falta arrumar as funcionalidades dos botões

--- src/livros.jsx
+++ src/livros.jsx
@@ -2,8 +2,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import "../src/scss/global.scss";
 
+// IMPORTS DE ESTILO E DE IMAGEM
 import S from './scss/styleComponents/routes/donated-book/donatedbook.module.scss';
 import Delete from './assets/routes-img/delete.png';
+import Edit from './assets/routes-img/edit.png'
 
 export default function ShowBook() {
     const [book, setBook] = useState([]);
@@ -22,16 +24,16 @@ export default function ShowBook() {
 
     const deleteLivro = async (id) => {
         const urlAPI = `https://api-livros-wtvn.onrender.com/donatedBooks/${id}`;
-      
+
         try {
-          await axios.delete(urlAPI); // Envia a requisição DELETE para a API
-          setBook(book.filter((item) => item.id !== id)); // Atualiza o estado para remover o livro excluído
-          console.log(`${id} - Este livro nos deu adeus!`);
+            await axios.delete(urlAPI); // Envia a requisição DELETE para a API
+            setBook(book.filter((item) => item.id !== id)); // Atualiza o estado para remover o livro excluído
+            console.log(`${id} - Este livro nos deu adeus!`);
         } catch (error) {
-          console.error("Temos certeza que este livro ainda está aqui!", error);
+            console.error("Temos certeza que este livro ainda está aqui!", error);
         }
-      };
-      
+    };
+
 
     useEffect(() => {
         getLivros();
@@ -41,20 +43,22 @@ export default function ShowBook() {
     return (
         <section className={S.sectionDonated}>
             <h2>Livros doados</h2>
-            <div>
+            <div className={S.divDonated}>
                 {book.map((book) => (
-                    <article key={book.id}>
-                        <figure>
-                            <img src={book.image_url} alt={book.title} />
-                        </figure>
-                        <article>
-                            <h3>{book.title}</h3>
-                            <h4>{book.author}</h4>
-                            <h5>{book.category}</h5>
-                            <button>
-                                <img src={Delete} alt="Imagem de uma lixeira onde se pode clicar para deletar o livro" />
+                    <article key={book.id} className={S.articleDonated}>
+                        <img src={book.image_url} alt={book.title} />
+                        <h3>{book.title}</h3>
+                        <h4>{book.author}</h4>
+                        <h5>{book.category}</h5>
+                        <div className={S.divBtnDonated}>
+                            <button className={S.btnDonated}>
+                                <img src={Edit} alt="Imagem de um lápis onde se pode clicar para editar o livro" />
+                                Editar
                             </button>
-                        </article>
+                            <button className={S.btnDonated}>
+                                <img src={Delete} alt="Imagem de uma lixeira onde se pode clicar para deletar o livro" /> Excluir
+                            </button>
+                        </div>
                     </article>
                 ))
                 }
